00c6f75a156e329459580b6e43debb3dbe2048dd
gabriel-severo <gabriel.ivi@hotmail.com>
2021-04-25 01:24:05 -0300

    Criado componente de projeto e removido exemplo no componente Projetos

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -1,26 +1,10 @@
 import styles from '../../styles/Projetos.module.css'
+import Projeto from '../components/Projeto'
 
 function Portfolio() {
     return (
         <div className={styles.projetos}>
-            <div className={styles.itemProjeto}>
-                <a href="#">
-                    <div className={styles.itemImagem}>
-                        <div className={styles.subInfo}>
-                            <i class="fas fa-info-circle"></i>
-                        </div>
-                        <img src="http://preview.hasthemes.com/portflow/images/portfolio/equal/1.jpg"></img>
-                    </div>
-                    <div className={styles.itemFooter}>
-                        <div className={styles.itemTitle}>
-                            Caneca
-                        </div>
-                        <div className={styles.itemDescricao}>
-                            Esta é uma caneca
-                        </div>
-                    </div>
-                </a>
-            </div>
+            <Projeto/>
         </div>
     )
 }
